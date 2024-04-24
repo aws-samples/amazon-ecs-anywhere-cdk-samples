b0fd9fb1f1b87504c4f8fac4cf4688af45485f03
gikwadr <gikwadr@amazon.com>
2024-04-24 12:18:20 +0530

    upgrade to cdk v2

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { EcsAnywhereStack } from '../lib/EcsAnywhereStack';
 
 const app = new cdk.App();
 const devEnv = {  
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-} 
-new EcsAnywhereStack(app, 'EcsAnywhereStack', {env: devEnv});
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  } 
+  new EcsAnywhereStack(app, 'EcsAnywhereStack', {env: devEnv});
